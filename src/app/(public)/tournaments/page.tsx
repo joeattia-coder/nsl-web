a1544d446ab5a63a0fbba3491b4c889a6927a56b
Joe Attia <joe.attia@gmail.com>
2026-03-08 22:59:49 -0400

    Add placeholder tournaments page

---
+++ src/app/(public)/tournaments/page.tsx
@@ -0,0 +1,8 @@
+export default function TournamentsPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Tournaments</h1>
+      <p>Tournament listings will appear here soon.</p>
+    </div>
+  );
+}
